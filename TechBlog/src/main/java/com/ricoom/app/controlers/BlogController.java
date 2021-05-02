74c78df23f1b8225b516b7b9ad463deb9b389d00
Admin <Admin@345242546-rt>
2021-05-02 18:20:33 +0300

    postgress configuration

--- TechBlog/src/main/java/com/ricoom/app/controlers/BlogController.java
+++ TechBlog/src/main/java/com/ricoom/app/controlers/BlogController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,27 +19,31 @@ import com.ricoom.app.services.BlogService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200")
 public class BlogController {
 	@Autowired
 	private BlogService blogService;
+
 	@PostMapping("/add")
-	public ResponseEntity<BlogPost>newBlog(@RequestBody BlogPost blog){
+	public ResponseEntity<BlogPost> newBlog(@RequestBody BlogPost blog) {
 		try {
-			BlogPost _blog=blogService.newBlog(blog);
-			return new  ResponseEntity<>(_blog,HttpStatus.CREATED);
+			BlogPost _blog = blogService.newBlog(blog);
+			return new ResponseEntity<>(_blog, HttpStatus.CREATED);
 		} catch (Exception e) {
-		      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
+
 	@GetMapping("/all")
- public ResponseEntity<List<BlogPost>> allBlogs(@RequestParam(required = false) String title){
-	 List<BlogPost> blogs=new ArrayList<BlogPost>();
-	 
-	 blogService.getAll(title).forEach(blogs::add);
-	 if (blogs.isEmpty()) {
-		 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-		 }
-	  return new ResponseEntity<>(blogs,HttpStatus.OK);
-	 
- }
+	public ResponseEntity<List<BlogPost>> allBlogs(@RequestParam(required = false) String title) {
+		List<BlogPost> blogs = new ArrayList<BlogPost>();
+
+		blogService.getAll(title).forEach(blogs::add);
+		if (blogs.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<>(blogs, HttpStatus.OK);
+
+	}
+
 }
--- TechBlog/src/main/java/com/ricoom/app/models/BlogPost.java
+++ TechBlog/src/main/java/com/ricoom/app/models/BlogPost.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
@@ -16,6 +17,7 @@ private long id;
 @NotNull
 private String blogTitle;
 @NotNull
+@Lob
 private String blogContent;
 
 private String publishedDate;
